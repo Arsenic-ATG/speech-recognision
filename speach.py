4c368d04954095de617c8b5e1733299ebfbb3b26
Roshan Kumar <roshaen09@gmail.com>
2020-12-06 19:10:53 +0530

    added voice

--- speach.py
+++ speach.py
@@ -1,4 +1,18 @@
 import speech_recognition as sr
+import pyttsx3
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+print(voices[1].id)
+engine.setProperty('voice',voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+
+
 
 # obtain audio from the microphone
 r = sr.Recognizer()
@@ -8,8 +22,10 @@ with sr.Microphone() as source:
 
 # recognize speech using Google
 try:
+    speak('you said " ' + r.recognize_google(audio) + ' "')
     print('you said " ' + r.recognize_google(audio) + ' "')
 except sr.UnknownValueError:
+    speak("Google could not understand audio")
     print("Google could not understand audio")
 except sr.RequestError as e:
     print("Google error; {0}".format(e))
